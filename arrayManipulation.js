a1107579d5ffe982acfb1b1dab82595ecb4368a1
Kelvin Ashong <kksashong@st.ug.edu.gh>
2024-05-17 16:37:43 +0000

    I have completed Task 1 printing out the new array and ready to push to github.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,11 +5,23 @@ function processArray(array){
        } else {
            return num * 3;
        }
-   });
-     
+   }); 
    }
-   
    var inputArray = [1, 2, 3, 4, 5];
    var resultArray = processArray(inputArray);
    console.log(resultArray); 
-   
+
+
+   function formatArrayStrings(strings,numbers){
+     return strings.map(function(str, index) {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+ }
+ var stringsArray = ["Math", "Hello WORLD", "lecture", "Indomie","bAbY"];
+ var numbersArray = inputArray;
+ var formattedArray = formatArrayStrings(stringsArray, numbersArray);
+ console.log(formattedArray);
